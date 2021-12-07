f2952864d5ec70b2fa49c87a45dea29678087452
Vinicius Gomes <neogedom@gmail.com>
2021-12-07 09:41:21 -0400

    Project created

---
+++ src/main/java/com/neogedom/professorvacancies/ProfessorVacanciesApplication.java
@@ -0,0 +1,13 @@
+package com.neogedom.professorvacancies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorVacanciesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessorVacanciesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/neogedom/professorvacancies/ProfessorVacanciesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neogedom.professorvacancies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorVacanciesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
