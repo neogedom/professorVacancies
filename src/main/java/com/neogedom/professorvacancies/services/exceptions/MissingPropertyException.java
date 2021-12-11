0e50172fb5c9eb24656d1efd3c1716a225d142f4
Vinicius Gomes <neogedom@gmail.com>
2021-12-11 15:34:13 -0400

    Initial configuration of Spring Security

---
+++ src/main/java/com/neogedom/professorvacancies/services/exceptions/MissingPropertyException.java
@@ -0,0 +1,5 @@
+package com.neogedom.professorvacancies.services.exceptions;
+
+public class MissingPropertyException extends RuntimeException {
+    public MissingPropertyException(String msg) {super(msg);}
+}
