0e58bd07050280acb2f51cda3e4ab94e8bebd068
Vinicius Gomes <neogedom@gmail.com>
2021-12-13 14:39:24 -0400

    profiles added to Professor and Aluno classes

--- src/main/java/com/neogedom/professorvacancies/domain/Aluno.java
+++ src/main/java/com/neogedom/professorvacancies/domain/Aluno.java
@@ -1,12 +1,15 @@
 package com.neogedom.professorvacancies.domain;
 
+import com.neogedom.professorvacancies.domain.enums.Perfil;
 import lombok.*;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @EqualsAndHashCode(callSuper = true)
 @Document(collection = "usuario") @Data @NoArgsConstructor
@@ -14,11 +17,17 @@ public class Aluno extends Usuario {
     @NonNull private String nome;
     @NonNull private String telefone;
     private List<Orientacao> orientacoes = new ArrayList<>();
+    private Set<Perfil> perfis = new HashSet<>();
 
     public Aluno(String id, @NonNull String nome, @NonNull String telefone,
     @NotNull String email, @NotNull String senha) {
         super(id, email, senha);
         this.nome = nome;
         this.telefone = telefone;
+        addPerfil(Perfil.ROLE_ALUNO);
+    }
+
+    public void addPerfil(Perfil perfil) {
+        perfis.add(perfil);
     }
 }
---
+++ src/main/java/com/neogedom/professorvacancies/domain/enums/Perfil.java
@@ -0,0 +1,6 @@
+package com.neogedom.professorvacancies.domain.enums;
+
+public enum Perfil {
+    ROLE_PROFESSOR,
+    ROLE_ALUNO
+}
